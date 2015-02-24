f93070f1abb8466a5393ca9b272f6a1d2f447699
Pusher Robot <jack@semantic-ui.com>
2015-02-24 12:10:36 -0500

    Updated component to version 1.10.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-list',
   summary : 'Semantic UI - List: Single component release',
-  version : '1.10.0',
+  version : '1.10.1',
   git     : 'git://github.com/Semantic-Org/UI-List.git',
 });
 
