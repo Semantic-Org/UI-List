af78ae243811f6dfa36d7cc3ae31732da65c79be
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:41:59 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-list',
   summary : 'Semantic UI - List: Single component release',
-  version : '2.2.9',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-List.git',
 });
 
